8747180af180d708d6623f16b68daa6d17185af7
ATM-TECH <ATM-TECH@users.noreply.github.com>
2026-09-03 04:17:01 +0530

    Use supplied artwork for installed app icon

--- apps/web/public/service-worker.js
+++ apps/web/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "aapoorti-business-connect-shell-v3";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/app-icon.svg"];
+const CACHE_NAME = "aapoorti-business-connect-shell-v4";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/app-icon-180.png", "/app-icon-192.png", "/app-icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
